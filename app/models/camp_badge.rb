1f22492dc473d651fe14ad7f8d829f5ff9a00764
Stephanie Ananth <stephkananth@gmail.com>
2019-04-13 04:20:12 -0400

    updated some model methods

--- app/models/camp_badge.rb
+++ app/models/camp_badge.rb
@@ -1,7 +1,10 @@
 class CampBadge < ApplicationRecord
+
   # relationships
   belongs_to :badge
   belongs_to :camp
+  has_many :camper_camp_badges
+  has_many :counselor_camp_badges
 
   # validations
   validates_presence_of :badge_id, :camp_id
