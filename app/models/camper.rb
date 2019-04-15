c0a8a4b1ee685ccb74699d94c925b90d5b219911
Tiffany Chen <thchen@andrew.cmu.edu>
2019-04-15 10:34:17 -0400

    show campers and counselors wip

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,5 +1,7 @@
 class Camper < ApplicationRecord
   belongs_to :parent
+  has_many :camp_registrations
+  has_many :camps, through: :camp_registrations
 
    def proper_name
     first_name + " " + last_name
--- app/models/parent.rb
+++ app/models/parent.rb
@@ -2,4 +2,8 @@ class Parent < ApplicationRecord
   # relationships
   belongs_to :user
   has_many :campers
+
+  def proper_name
+    self.user.first_name + " " + self.user.last_name
+  end
 end
