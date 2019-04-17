98928f2fbf9e7d88204839f8cf1105da16fbbd38
Stephanie Ananth <stephkananth@gmail.com>
2019-04-17 12:16:21 -0400

    reformat code

--- app/models/camper_camp_badge_task.rb
+++ app/models/camper_camp_badge_task.rb
@@ -10,8 +10,8 @@ class CamperCampBadgeTask < ApplicationRecord
   validates_presence_of :task_id
 
   # scopes
-  scope :completed, -> { where(completed: true) }
-  scope :incomplete, -> { where(completed: false) }
+  scope :completed, -> {where(completed: true)}
+  scope :incomplete, -> {where(completed: false)}
 
   # callbacks
   around_update :mark_as_completed
