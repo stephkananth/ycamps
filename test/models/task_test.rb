bcd1b248f3ff4e250e1014baf7054cbed1cd0904
Stephanie Ananth <stephkananth@gmail.com>
2019-04-20 12:14:05 -0400

    added in_order scope to task

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -2,6 +2,27 @@ require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
   should belong_to(:badge)
-
   should validate_presence_of(:badge_id)
+
+  context 'within context' do
+    setup do
+      create_generic_skill
+      create_generic_badges
+      create_generic_tasks
+    end
+
+    teardown do
+      delete_generic_tasks
+      delete_generic_badges
+      delete_generic_skill
+    end
+
+    should 'show that in alphabetical scope works' do
+      assert_equal ['Task 1', 'Task 2', 'Task 3', 'Task 4'], Task.all.alphabetical.map(&:name)
+    end
+
+    should 'show that in order scope works' do
+      assert_equal ['Task 1', 'Task 2', 'Task 3', 'Task 4'], Task.all.in_order.map(&:name)
+    end
+  end
 end
--- test/sets/tasks_contexts.rb
+++ test/sets/tasks_contexts.rb
@@ -7,10 +7,10 @@ module Contexts
     end
 
     def create_generic_tasks
-      @task1 = FactoryBot.create(:task, badge: @badge3a, name: 'Task 1')
-      @task2 = FactoryBot.create(:task, badge: @badge3a, name: 'Task 2')
-      @task3 = FactoryBot.create(:task, badge: @badge3a, name: 'Task 3')
-      @task4 = FactoryBot.create(:task, badge: @badge3a, name: 'Task 4')
+      @task1 = FactoryBot.create(:task, badge: @badge3a, name: 'Task 1', order: 1)
+      @task2 = FactoryBot.create(:task, badge: @badge3a, name: 'Task 2', order: 2)
+      @task3 = FactoryBot.create(:task, badge: @badge3a, name: 'Task 3', order: 3)
+      @task4 = FactoryBot.create(:task, badge: @badge3a, name: 'Task 4', order: 4)
     end
 
     def delete_generic_tasks
