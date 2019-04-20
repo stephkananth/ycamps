826972eae51f1305d54d84e4b5e8141282c42336
Stephanie Ananth <stephkananth@gmail.com>
2019-04-20 13:08:10 -0400

    Update assign_milestone_helper.rb

--- app/helpers/assign_milestone_helper.rb
+++ app/helpers/assign_milestone_helper.rb
@@ -4,7 +4,7 @@ module AssignMilestoneHelper
       ccb = CamperCampBadge.where(camp_badge_id: camp_badge_id, camper_id: camper).first
       camper_camp_badge_task = CamperCampBadgeTask.where(task_id: task_id, camper_camp_badge_id: ccb.id).first
       camper_camp_badge_task.completed = true
-      camper_camp_badge_task.save
+      camper_camp_badge_task.save!
     end
   end
 end
